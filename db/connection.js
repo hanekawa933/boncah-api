dcd0b376a80e2fdcb80082c5c6adef234c749138
hanekawa933 <iqbalkorompiz@gmail.com>
2021-12-16 04:39:47 +0700

    ANJING LO

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,31 @@
 const { Sequelize } = require("sequelize");
 const dbConf = require("../config/config.json")["development"];
 
-const options = {
-  define: {
-    freezeTableName: true,
-  },
-  dialectOptions: {
-    dateStrings: true,
-    typeCast: function (field, next) {
-      // for reading from database
-      if (field.type === "DATETIME") {
-        return field.string();
-      }
-      return next();
-    },
-  },
-};
+// const options = {
+//   define: {
+//     freezeTableName: true,
+//   },
+//   dialectOptions: {
+//     dateStrings: true,
+//     typeCast: function (field, next) {
+//       // for reading from database
+//       if (field.type === "DATETIME") {
+//         return field.string();
+//       }
+//       return next();
+//     },
+//   },
+// };
 
-const config = Object.assign(dbConf, options);
-const db = new Sequelize(config);
+const db = new Sequelize(
+  "n1007420_boncah",
+  "n1007420_erorsetneg",
+  "@Thisiserorsetnegcpanelpassword999",
+  {
+    host: "erorsetneg.com",
+    dialect: "mysql",
+  }
+);
 
 const connection = () => {
   try {
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const { connection } = require("./db/connection");
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "ok" });
+  res.send("KONTOL");
 });
-
-const port = process.env.PORT || 8000;
 
 app.listen(port, () =>
   console.log(`Server running on ${port}, http://localhost:${port}`)
