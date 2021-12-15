e8c29f4e109f6d7e90a0f5dda8de6c61015feea4
hanekawa933 <iqbalkorompiz@gmail.com>
2021-12-16 05:10:11 +0700

    ALLO

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ app.use(errorHandlers);
 
 const port = process.env.PORT || 8000;
 
-app.listen(process.env.PORT, () =>
+app.listen(port, () =>
   console.log(`Server running on ${port}, http://localhost:${port}`)
 );
