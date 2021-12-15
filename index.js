4a32254fe805debb9f2790cc55ef00709a404c23
hanekawa933 <iqbalkorompiz@gmail.com>
2021-12-16 04:29:35 +0700

    ANJING LO

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const { connection } = require("./db/connection");
 
+connection();
+
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
