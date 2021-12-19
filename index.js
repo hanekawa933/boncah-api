6533ea8a359465cbf8f5a18b40d462ffa7a45dcb
hanekawa933 <iqbalkorompiz@gmail.com>
2021-12-20 06:08:20 +0700

    commit

--- index.js
+++ index.js
@@ -22,15 +22,14 @@ const pengeluaran = require("./routes/api/pengeluaran");
 const cors = require("cors");
 
 const { notFound, errorHandlers } = require("./middleware/errorMessage");
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
 app.use(morgan("tiny"));
 app.use(helmet());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 connection();
 
